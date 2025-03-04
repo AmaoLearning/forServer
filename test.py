900e319526f9c48809f85b3041b7bc13766b8ddc
AmaoLearning <darksoul1remaked@outlook.com>
2025-03-04 22:02:58 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from d2l import tensorflow as d2l
+
+# 卷积块
+class ConvBlock(tf.keras.layers.Layer):
+    def __init__(self, num_channels):
+        super(ConvBlock, self).__init__()
+        self.bn = tf.keras.layers.BatchNormalization()
+        self.relu = tf.keras.layers.ReLU()
+        self.conv = tf.keras.layers.Conv2D(
+            filters=num_channels, kernel_size=(3, 3), padding='same')
+
+        self.listLayers = [self.bn, self.relu, self.conv]
+
+    def call(self, x):
+        y = x
+        for layer in self.listLayers.layers:
+            y = layer(y)
+        y = tf.keras.layers.concatenate([x,y], axis=-1)
+        return y
+
+# 稠密块
+class DenseBlock(tf.keras.layers.Layer):
+    def __init__(self, num_convs, num_channels):
+        super(DenseBlock, self).__init__()
+        self.listLayers = []
+        for _ in range(num_convs):
+            self.listLayers.append(ConvBlock(num_channels))
+
+    def call(self, x):
+        for layer in self.listLayers.layers:
+            x = layer(x) # 每个层
+        return x
+
+# 过渡层
+class TransitionBlock(tf.keras.layers.Layer):
+    def __init__(self, num_channels, **kwargs):
+        super(TransitionBlock, self).__init__(**kwargs)
+        self.batch_norm = tf.keras.layers.BatchNormalization()
+        self.relu = tf.keras.layers.ReLU()
+        self.conv = tf.keras.layers.Conv2D(num_channels, kernel_size=1)
+        self.avg_pool = tf.keras.layers.AvgPool2D(pool_size=2, strides=2)
+
+    def call(self, x):
+        x = self.batch_norm(x)
+        x = self.relu(x)
+        x = self.conv(x)
+        return self.avg_pool(x)
+
+def block_1():
+    return tf.keras.Sequential([
+       tf.keras.layers.Conv2D(64, kernel_size=7, strides=2, padding='same'),
+       tf.keras.layers.BatchNormalization(),
+       tf.keras.layers.ReLU(),
+       tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='same')])
+
+def block_2():
+    net = block_1()
+    # num_channels为当前的通道数
+    num_channels, growth_rate = 64, 32
+    num_convs_in_dense_blocks = [4, 4, 4, 4]
+
+    for i, num_convs in enumerate(num_convs_in_dense_blocks):
+        net.add(DenseBlock(num_convs, growth_rate))
+        # 上一个稠密块的输出通道数
+        num_channels += num_convs * growth_rate
+        # 在稠密块之间添加一个转换层，使通道数量减半
+        if i != len(num_convs_in_dense_blocks) - 1:
+            num_channels //= 2
+            net.add(TransitionBlock(num_channels))
+    return net
+
+def net():
+    net = block_2()
+    net.add(tf.keras.layers.BatchNormalization())
+    net.add(tf.keras.layers.ReLU())
+    net.add(tf.keras.layers.GlobalAvgPool2D())
+    net.add(tf.keras.layers.Flatten())
+    net.add(tf.keras.layers.Dense(10))
+    return net
+
+lr, num_epochs, batch_size = 0.1, 10, 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=96)
+d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
